dde18dd0f2764c648b0a698a91dc8f3a40ff4320
Konovalov Kirill <kirkonowalow@gmail.com>
2024-04-19 09:41:21 +0300

    done

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -1,13 +1,20 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
     private val context: Context
 ) {
+    private fun getString(): String {
+        val ar = context.resources.getStringArray(R.array.local_cat_facts)
+        return ar[Random.nextInt(ar.size)]
+    }
 
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact так,
@@ -15,7 +22,9 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        return Single.create { e ->
+            e.onSuccess(Fact(getString()))
+        }
     }
 
     /**
@@ -24,7 +33,10 @@ class LocalCatFactsGenerator(
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        val success =
+            Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+        return Flowable.interval(0, 2000, TimeUnit.MILLISECONDS)
+            .map { success }
+            .distinctUntilChanged()
     }
 }
